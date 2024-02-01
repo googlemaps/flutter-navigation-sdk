4f9fa0e9affc80b0a00c3dbb221d4d3c69c6c1ef
vessaas <vesse.saastamoinen@codemate.com>
2024-02-01 21:46:08 +0700

    chore: Add CI configuration

---
+++ tools/test-ios.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -e
+
+DEVICE_NAME=${DEVICE:-'iPhone 14 Pro'} # Default to 'iPhone 14 Pro' if no argument is provided
+
+# Navigate to the ios directory and run xcodebuild with the provided device name
+cd ios && xcodebuild test \
+            -workspace Runner.xcworkspace \
+            -scheme Runner \
+            -only-testing RunnerTests \
+            -configuration Debug \
+            -sdk iphoneos -destination "platform=iOS Simulator,name=$DEVICE_NAME" \
+            -derivedDataPath ../build/ios_unit
