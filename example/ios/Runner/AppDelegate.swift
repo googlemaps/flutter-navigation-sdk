f53f7f4d5be01f4c165d7434fb8de9e286483f7f
Ville Välimaa <ville.valimaa@codemate.com>
2024-10-21 16:33:17 +0800

    feat: create separate ios build target for carplay

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -17,39 +17,80 @@ import Flutter
 import GoogleMaps
 import GoogleNavigation
 
+#if CARPLAY
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    print("HAS CARPLAY")
     // 1. Try to find the Maps API key from the environment variables.
     // 2. Try to find the Maps API key from the Dart defines.
     // 3. Use the default Maps API key "YOUR_API_KEY".
     var mapsApiKey = ProcessInfo.processInfo.environment["MAPS_API_KEY"] ?? findMapApiKeyFromDartDefines("MAPS_API_KEY") ?? ""
     if (mapsApiKey.isEmpty) {
-        mapsApiKey = "YOUR_API_KEY"
+      mapsApiKey = "YOUR_API_KEY"
     }
     GMSServices.provideAPIKey(mapsApiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-  
+
   // Helper function to find the Maps API key from the Dart defines
   private func findMapApiKeyFromDartDefines(_ defineKey: String) -> String? {
-      if (Bundle.main.infoDictionary!["DART_DEFINES"] == nil) {
-          return nil
-      }
-
-      let dartDefinesString = Bundle.main.infoDictionary!["DART_DEFINES"] as! String
-      let base64EncodedDartDefines = dartDefinesString.components(separatedBy: ",")
-      for base64EncodedDartDefine in base64EncodedDartDefines {
-          let decoded = String(data: Data(base64Encoded: base64EncodedDartDefine)!, encoding: .utf8)!
-          let values = decoded.components(separatedBy: "=")
-          if (values[0] == defineKey && values.count == 2) {
-              return values[1]
-          }
-      }
+    if (Bundle.main.infoDictionary!["DART_DEFINES"] == nil) {
       return nil
+    }
+
+    let dartDefinesString = Bundle.main.infoDictionary!["DART_DEFINES"] as! String
+    let base64EncodedDartDefines = dartDefinesString.components(separatedBy: ",")
+    for base64EncodedDartDefine in base64EncodedDartDefines {
+      let decoded = String(data: Data(base64Encoded: base64EncodedDartDefine)!, encoding: .utf8)!
+      let values = decoded.components(separatedBy: "=")
+      if (values[0] == defineKey && values.count == 2) {
+        return values[1]
+      }
+    }
+    return nil
   }
 }
+#else
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    print("NO CARPLAY")
+    // 1. Try to find the Maps API key from the environment variables.
+    // 2. Try to find the Maps API key from the Dart defines.
+    // 3. Use the default Maps API key "YOUR_API_KEY".
+    var mapsApiKey = ProcessInfo.processInfo.environment["MAPS_API_KEY"] ?? findMapApiKeyFromDartDefines("MAPS_API_KEY") ?? ""
+    if (mapsApiKey.isEmpty) {
+      mapsApiKey = "YOUR_API_KEY"
+    }
+    GMSServices.provideAPIKey(mapsApiKey)
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // Helper function to find the Maps API key from the Dart defines
+  private func findMapApiKeyFromDartDefines(_ defineKey: String) -> String? {
+    if (Bundle.main.infoDictionary!["DART_DEFINES"] == nil) {
+      return nil
+    }
+
+    let dartDefinesString = Bundle.main.infoDictionary!["DART_DEFINES"] as! String
+    let base64EncodedDartDefines = dartDefinesString.components(separatedBy: ",")
+    for base64EncodedDartDefine in base64EncodedDartDefines {
+      let decoded = String(data: Data(base64Encoded: base64EncodedDartDefine)!, encoding: .utf8)!
+      let values = decoded.components(separatedBy: "=")
+      if (values[0] == defineKey && values.count == 2) {
+        return values[1]
+      }
+    }
+    return nil
+  }
+}
+#endif
