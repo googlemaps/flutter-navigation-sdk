3c794fda18bcd5a8c279df55946ed7dfb13adfdc
Joonas Kerttula <joonas.kerttula@codemate.com>
2025-04-10 10:45:22 +0300

    feat: switch from PlatformViewLink to AndroidView

--- lib/src/google_navigation_flutter_android.dart
+++ lib/src/google_navigation_flutter_android.dart
@@ -85,38 +85,18 @@ class GoogleMapsNavigationAndroid extends GoogleMapsNavigationPlatform {
       initializationOptions,
     );
 
-    return PlatformViewLink(
+    return AndroidView(
       viewType: viewType,
-      surfaceFactory:
-          (BuildContext context, PlatformViewController controller) {
-        return AndroidViewSurface(
-          controller: controller as AndroidViewController,
-          gestureRecognizers: initializationOptions.gestureRecognizers,
-          hitTestBehavior: PlatformViewHitTestBehavior.opaque,
-        );
+      onPlatformViewCreated: (int viewId) async {
+        // On Android the map is initialized asyncronously.
+        // Wait map to be ready before calling [onMapReady] callback
+        await viewAPI.awaitMapReady(viewId: viewId);
+        onMapReady(viewId);
       },
-      onCreatePlatformView: (PlatformViewCreationParams params) {
-        return PlatformViewsService.initSurfaceAndroidView(
-          id: params.id,
-          viewType: viewType,
-          layoutDirection: initializationOptions.layoutDirection,
-          creationParams: creationParams.encode(),
-          creationParamsCodec: const StandardMessageCodec(),
-          onFocus: () {
-            params.onFocusChanged(true);
-          },
-        )
-          ..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)
-          ..addOnPlatformViewCreatedListener(
-            (int viewId) async {
-              // On Android the map is initialized asyncronously.
-              // Wait map to be ready before calling [onMapReady] callback
-              await viewAPI.awaitMapReady(viewId: viewId);
-              onMapReady(viewId);
-            },
-          )
-          ..create();
-      },
+      gestureRecognizers: initializationOptions.gestureRecognizers,
+      layoutDirection: initializationOptions.layoutDirection,
+      creationParams: creationParams.encode(),
+      creationParamsCodec: const StandardMessageCodec(),
     );
   }
 
