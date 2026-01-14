61c2341d66fcd4f4890d0adff51108cc79efa32c
Samuel Dubovec <samodubovec@gmail.com>
2026-01-14 16:16:24 +0100

    test: adding projection tests

---
+++ test/types/screen_coordinate_test.dart
@@ -0,0 +1,30 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_navigation_flutter/google_navigation_flutter.dart';
+import 'package:google_navigation_flutter/src/method_channel/method_channel.dart';
+
+void main() {
+  group('ScreenCoordinate tests', () {
+    test('DTO conversion preserves values', () {
+      const ScreenCoordinate original = ScreenCoordinate(x: 123.45, y: 678.90);
+      final ScreenCoordinateDto dto = original.toDto();
+      final ScreenCoordinate converted = dto.toScreenCoordinate();
+
+      expect(converted.x, original.x);
+      expect(converted.y, original.y);
+    });
+  });
+}
